a9233a662ca8396affce4deb28757a4e760af2d4
nandzh <54406485+nandzh@users.noreply.github.com>
2025-07-03 17:18:42 +0200

    Simple Local Database
    Added a local database and a general Brand class to test basic functionality on

---
+++ SustainabilityWebApp/Migrations/20250703151638_InitialCreate.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace SustainabilityWebApp.Migrations
+{
+    /// <inheritdoc />
+    public partial class InitialCreate : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Brand",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Name = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    FoundedDate = table.Column<DateOnly>(type: "date", nullable: false),
+                    Score = table.Column<float>(type: "real", nullable: false),
+                    AveragePrice = table.Column<decimal>(type: "decimal(18,2)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Brand", x => x.Id);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Brand");
+        }
+    }
+}
---
+++ SustainabilityWebApp/Models/Brand.cs
@@ -0,0 +1,20 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+
+namespace SustainabilityWebApp.Models
+{
+    public class Brand
+    {
+        public int Id { get; set; }
+        public string? Name { get; set; }
+        public DateOnly FoundedDate { get; set; }
+
+        public float Score { get; set; }
+
+        [DataType(DataType.Currency)]
+        [Column(TypeName = "decimal(18, 2)")]
+        public decimal AveragePrice { get; set; }
+
+    }
+}
